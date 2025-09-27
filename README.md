d965b5324c017c6b18cb3d6e429673d872414ecf
Philipp Roshchin <r.phil@yandex.ru>
2025-09-27 14:40:51 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # project-1_Roshchin_M25-555
+
+To install this package run:
+
+```bash
+pip install .
+```
+
+To run this package use:
+
+```bash
+conda activate labyrinth_game
+
+labyrinth_game
+```
+
+Development:
+
+```bash
+conda env create -f ./env.yml
+conda activate labyrinth_game
+```
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+
+setup(
+    name="labyrinth_game",
+    version="0.1.0",
+    author="Philipp Roschin",
+    author_email="r.phil@yandex.ru",
+    description="Labyrinth game.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "labyrinth_game=labyrinth_game.main:main",
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12",
+)
