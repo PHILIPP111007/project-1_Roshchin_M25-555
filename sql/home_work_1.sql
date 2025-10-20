7800e02cef25aaa97078adfb1cdb7e23a40a8034
Philipp Roshchin <r.phil@yandex.ru>
2025-10-20 14:30:16 +0300

    update

--- sql/home_work_1.sql
+++ sql/home_work_1.sql
@@ -35,60 +35,7 @@ CREATE TABLE rooms (
 );
 
 -- Таблица стеллажей
-CREATE TABLE racks (-- Таблица клиентов
-CREATE TABLE clients (
-    id INTEGER PRIMARY KEY,
-    name_of_a_legal_entity TEXT NOT NULL,
-    bank_details TEXT NOT NULL
-);
-
--- Таблица помещений
-CREATE TABLE rooms (
-    id INTEGER PRIMARY KEY,
-    room_name TEXT NOT NULL,
-    useful_volume REAL NOT NULL,
-    min_temperature REAL,
-    max_temperature REAL,
-    min_humidity REAL,
-    max_humidity REAL
-);
-
--- Таблица стеллажей
 CREATE TABLE racks (
-    id INTEGER PRIMARY KEY,
-    room_id INTEGER NOT NULL,
-    rack_number TEXT NOT NULL,
-    storage_spaces_count INTEGER NOT NULL,
-    space_height REAL NOT NULL,
-    space_width REAL NOT NULL,
-    space_length REAL NOT NULL,
-    max_total_load REAL NOT NULL,
-    FOREIGN KEY (room_id) REFERENCES rooms(id),
-    UNIQUE (rack_number, room_id)
-);
-
--- Таблица товаров
-CREATE TABLE products (
-    id INTEGER PRIMARY KEY,
-    height REAL NOT NULL,
-    width REAL NOT NULL,
-    length REAL NOT NULL,
-    weight REAL NOT NULL,
-    admission_date TEXT NOT NULL,
-    contract_number TEXT NOT NULL,
-    client_id INTEGER NOT NULL,
-    contract_expiration_date TEXT NOT NULL,
-    min_temperature REAL,
-    max_temperature REAL,
-    min_humidity REAL,
-    max_humidity REAL,
-    rack_id INTEGER NOT NULL,
-    position_number INTEGER NOT NULL,
-    FOREIGN KEY (client_id) REFERENCES clients(id),
-    FOREIGN KEY (rack_id) REFERENCES racks(id),
-    UNIQUE (rack_id, position_number)
-);
-
     id INTEGER PRIMARY KEY,
     room_id INTEGER NOT NULL,
     rack_number TEXT NOT NULL,
